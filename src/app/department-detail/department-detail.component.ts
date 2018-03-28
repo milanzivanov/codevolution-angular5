27b0c3f22274af7bf892ce96914cc6ee4ef6a9b8
milan.zivanov <milan.zivanov@gmail.com>
2018-03-28 18:08:19 +0200

    paramMap Observable

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -8,13 +8,26 @@ import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 })
 export class DepartmentDetailComponent implements OnInit {
   public departmentId;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     // parseInt(string: string, radix?: number): number;
     // const n = parseInt((1.2).toString(), 10);
-    const id = parseInt(this.route.snapshot.paramMap.get('id').toString(), 10);
-    this.departmentId = id;
+    // const id = parseInt(this.route.snapshot.paramMap.get('id').toString(), 10);
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id = parseInt(params.get('id'), 10);
+      this.departmentId = id;
+    });
   }
 
+  goPrev() {
+    const prevId = this.departmentId - 1;
+    this.router.navigate(['/departments', prevId]);
+  }
+
+  goNext() {
+    const nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
 }
