8aa57d32c8daf4a3306d4526ae92357426217990
milan.zivanov <milan.zivanov@gmail.com>
2018-03-29 17:34:56 +0200

    Interpolation

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+   public name = 'Milandarius';
+   public siteUrl = window.location.href;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser() {
+    return `Hello ${this.name}`;
+  }
+
+}
