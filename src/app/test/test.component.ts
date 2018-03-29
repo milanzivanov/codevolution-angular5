2bdfb8e6e0110f4943e790c41ad2578082a9787b
milan.zivanov <milan.zivanov@gmail.com>
2018-03-29 18:48:54 +0200

    Property Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-   public name = 'Milandarius';
-   public siteUrl = window.location.href;
+  public name = 'Milandarius';
+  public siteUrl = window.location.href;
+
+  public myId = 'testId';
+  public isDisabled = false;
 
   constructor() { }
 
