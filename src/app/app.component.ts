2c392bcee5a9ff6dbc51854dfacdee2c83276911
milan.zivanov <milan.zivanov@gmail.com>
2018-06-23 13:36:24 +0200

    Template Driven Form to be continued

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Milan', 'm@m.com', 1234567, '', 'morning', true);
 }
