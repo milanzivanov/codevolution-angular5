7786da3c904ed7ae2d59598f88cd6effdfba44db
milan.zivanov <milan.zivanov@gmail.com>
2018-06-25 16:56:17 +0200

    Displaying Error Messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ export class AppComponent {
 
   topics = ['Angular', 'React', 'Vue'];
 
-  userModel = new User('', 'm@m.com', 1234567890, '', 'morning', true);
+  userModel = new User('Milan', 'm@m.com', 123456789, '', 'morning', true);
 }
